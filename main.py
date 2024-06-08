b28c3ec2ed25a6a27f41131234df0fb29ad50ac4
herewegoagainwow <prakasharunav12@gmail.com>
2024-06-08 11:25:33 +0530

    Interim commit

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainigPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainigPipeline
+from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 from textSummarizer.logging import logger
 
 
 
 STAGE_NAME = 'Data Ingestion Stage'
 try:
-    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
     data_ingestion = DataIngestionTrainigPipeline()
     data_ingestion.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<< \n\n x==========x")
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<< \n\n x==========x")
 except Exception as e:
     logger.exception(e)
     raise e
@@ -17,10 +18,23 @@ except Exception as e:
 
 STAGE_NAME = 'Data Validation Stage'
 try:
-    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
     data_validation = DataValidationTrainigPipeline()
     data_validation.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<< \n\n x==========x")
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<< \n\n x==========x")
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.main()
+   logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
